36e12bde21466d902417377ce6573e3f6bb2efe2
safiyy-a <safiyyaahmed05@gmail.com>
2025-05-11 11:28:07 +0100

    cid

--- web-app/main.js
+++ web-app/main.js
@@ -1,1 +1,14 @@
 import R from "./ramda.js";
+import candycrush from "./candycrush.js";
+
+
+// String literals.
+const image_sources = [
+    "./assets/image.png"
+];
+
+const image_alts = [
+    "image"
+];
+
+// put more string literals here eg result texts, player types
